75b400a1e43220e03cb3995c9da8ba9358c76c6d
Dimitar Rachev <DesolateBG@gmail.com>
2022-04-21 00:25:41 +0300

    Stage 2/6 completed

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -1,14 +1,22 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        String msg = "we found a treasure!";
+        Scanner scanner = new Scanner(System.in);
+        String msg = scanner.nextLine();
+        int key = Integer.parseInt(scanner.nextLine());
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < msg.length(); i++) {
             char curr = msg.charAt(i);
             if (curr >= 97 && curr <= 122) {
-                curr = (char) ((122 + 97) - curr);
+                curr = (char) (curr + key);
+                if (curr > 122) {
+                    curr = (char) (curr - 26);
+                }
             }
             sb.append(curr);
         }
